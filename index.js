3a7c5551fc8d44dac7c0873f70650f7417a9dc4a
PESD <102270504+JEMSHS@users.noreply.github.com>
2022-11-22 18:36:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const nums = '0123456789';
 
 const alphabet = katakana + latin + nums;
